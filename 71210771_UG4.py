aaece96bfe8d1131129cc2ceeb29f0f6d34e562f
CHRadyatma <93330711+7121christo@users.noreply.github.com>
2022-09-19 19:57:07 +0700

    Add files via upload

---
+++ 71210771_UG4.py
@@ -0,0 +1,48 @@
+#71210771 - CHRISTOPHORUS ADYATMA WAHYU SETYA NAYOTTAMA
+
+'''
+import json
+data={}
+with open('mahasiswa.json', 'r') as datafile:
+    fields=['']
+'''
+'''
+data={}
+data['hanif']=[]
+data['biodata'].append(
+    {
+    data['hobi'].append({'membaca','menyanyi','memasak'}),
+    data['prestasi'].append({"Sand Box Hackathon 1st Winner - Samsan Tech"})
+})
+'''
+'''
+data={}
+data[f'nama']=[]
+data['biodata'].append({
+    data['hobi'].append({})
+
+})
+'''
+
+jumlahmhs = int(input("Masukkan jumlah mahasiswa baru : "))
+
+for i in range(jumlahmhs):
+    nama=str(input("Masukkan nama Anda : "))
+    jumlahhobi=int(input("Masukkan jumlah hobi : "))
+    for i in range(jumlahhobi):
+        hobi=str(input("Masukan Hobi : "))
+    prestasi=input("Masukkan Prestasi Anda : ")
+    print("=== Data berhasil ditambahkan ===")
+
+#file=open('mahasiswi.json', 'a')
+#file.write()
+#file.close
+'''
+def writefile(baru, filename='mahasiswi.json'):
+    with open(filename, 'r+') as file:
+        filedata=json.load(file)
+        filedata[nama].append(baru)
+        file.seek(0)
+        json.dump(filedata,file,)
+
+'''
